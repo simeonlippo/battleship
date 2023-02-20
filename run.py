f8ab4b63dda408c04e897b879e0a5c3d59e128c3
simeonlippo <simeonlippo@gmail.com>
2023-02-20 16:36:38 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Minimal requirments: Show board, players shot, 
+check hit, miss or sink in loop.
+"""
+
+#Board
+print("   0  1  2  3  4  5  6  7  8  9")
+for coordinate in range(10):
+    print(coordinate, " - " *10)
