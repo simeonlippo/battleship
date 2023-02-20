314636539f9cfac6b4a60843a671b66c7f79da71
simeonlippo <simeonlippo@gmail.com>
2023-02-20 16:57:00 +0000

    add improved board structure

--- run.py
+++ run.py
@@ -1,9 +1,22 @@
 """
 Minimal requirments: Show board, players shot, 
 check hit, miss or sink in loop.
+
+Board needs to be interactive, change "coordinate" to "x" as in "xy graph"
 """
 
 #Board
+shot_hit = 21
+coordinate = 0
+print("            Battleship")
 print("   0  1  2  3  4  5  6  7  8  9")
-for coordinate in range(10):
-    print(coordinate, " - " *10)
+
+for x in range(10):
+    row = ""
+    for y in range(10):
+        character = " - "
+        if coordinate == 21:
+            character = " x "
+        row += character
+        coordinate += 1
+    print(x, row)
