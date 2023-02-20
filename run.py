5d644a500db2e3b256f8680871dfafd7e1b50015
simeonlippo <simeonlippo@gmail.com>
2023-02-20 17:09:28 +0000

    add 'hit', 'miss' and 'sink' symbols and make board more interactive

--- run.py
+++ run.py
@@ -6,17 +6,26 @@ Board needs to be interactive, change "coordinate" to "x" as in "xy graph"
 """
 
 #Board
-shot_hit = 21
-coordinate = 0
-print("            Battleship")
-print("   0  1  2  3  4  5  6  7  8  9")
+def show_board(shot_hit, shot_miss, shot_sink):
+    coordinate = 0
+    print("            Battleship")
+    print("   0  1  2  3  4  5  6  7  8  9")
 
-for x in range(10):
-    row = ""
-    for y in range(10):
-        character = " - "
-        if coordinate == 21:
-            character = " x "
-        row += character
-        coordinate += 1
-    print(x, row)
+    for x in range(10):
+        row = ""
+        for y in range(10):
+            character = " - "
+            if coordinate in shot_miss:
+                character = " o "
+            elif coordinate in shot_hit:
+                character = " x "
+            elif coordinate in shot_sink:
+                character = " ! "
+            row += character
+            coordinate += 1
+        print(x, row)
+
+shot_hit = [21, 22]
+shot_miss = [18, 19, 20]
+shot_sink = [23]
+show_board(shot_hit, shot_miss, shot_sink)
